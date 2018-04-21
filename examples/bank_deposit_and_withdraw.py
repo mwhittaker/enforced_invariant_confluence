7bb880329ae80595af930169a4a2b72a804bd3e5
Michael Whittaker <mjwhittaker@berkeley.edu>
2018-04-21 10:48:05 -0700

    Added escrow!

--- examples/bank_deposit_and_withdraw.py
+++ examples/bank_deposit_and_withdraw.py
@@ -21,6 +21,7 @@ p2 = checker.int_max('p2', 0)
 m0 = checker.int_max('m0', 0)
 m1 = checker.int_max('m1', 0)
 m2 = checker.int_max('m2', 0)
+
 checker.add_invariant('p0_geq_0', p0 >= 0)
 checker.add_invariant('p1_geq_0', p1 >= 0)
 checker.add_invariant('p2_geq_0', p2 >= 0)
@@ -28,10 +29,44 @@ checker.add_invariant('m0_geq_0', m0 >= 0)
 checker.add_invariant('m1_geq_0', m1 >= 0)
 checker.add_invariant('m2_geq_0', m2 >= 0)
 checker.add_invariant('x_geq_0', ((p0 + p1 + p2) - (m0 + m1 + m2)) >= 0)
-for i in range(10):
-    checker.add_transaction(f'x0_inc_{i}', [p0.assign(p0 + i)])
-    checker.add_transaction(f'x1_inc_{i}', [p1.assign(p1 + i)])
-    checker.add_transaction(f'x2_inc_{i}', [p2.assign(p2 + i)])
-    checker.add_transaction(f'x0_dec_{i}', [m0.assign(m0 + i)])
-    checker.add_transaction(f'x1_dec_{i}', [m1.assign(m1 + i)])
-    checker.add_transaction(f'x2_dec_{i}', [m2.assign(m2 + i)])
+
+checker.add_transaction('x0_inc', [p0.assign(p0 + 1)])
+checker.add_transaction('x1_inc', [p1.assign(p1 + 1)])
+checker.add_transaction('x2_inc', [p2.assign(p2 + 1)])
+checker.add_transaction('x0_dec', [m0.assign(m0 + 1)])
+checker.add_transaction('x1_dec', [m1.assign(m1 + 1)])
+checker.add_transaction('x2_dec', [m2.assign(m2 + 1)])
+
+if isinstance(checker, StratifiedChecker):
+    def add_base_invariants(stratum: Stratum) -> None:
+        stratum.add_invariant(p0 >= 0)
+        stratum.add_invariant(p1 >= 0)
+        stratum.add_invariant(p2 >= 0)
+        stratum.add_invariant(m0 >= 0)
+        stratum.add_invariant(m1 >= 0)
+        stratum.add_invariant(m2 >= 0)
+        stratum.add_invariant(((p0 + p1 + p2) - (m0 + m1 + m2)) >= 0)
+
+    escrowed = checker.stratum('escrowed')
+    add_base_invariants(escrowed)
+    for p in [p0, p1, p2]:
+        escrowed.add_invariant(p >= 10)
+    for m in [m0, m1, m2]:
+        escrowed.add_invariant(m <= 10)
+    for t in ['x0_inc', 'x1_inc', 'x2_inc', 'x0_dec', 'x1_dec', 'x2_dec']:
+        escrowed.add_transaction(t)
+
+    otherwise = checker.stratum('otherwise')
+    add_base_invariants(otherwise)
+    for t in ['x0_inc', 'x1_inc', 'x2_inc']:
+        otherwise.add_transaction(t)
+
+checker.check()
+
+if isinstance(checker, StratifiedChecker):
+    m0_lhs, m0_rhs = otherwise.checker.split(m0)
+    m1_lhs, m1_rhs = otherwise.checker.split(m1)
+    m2_lhs, m2_rhs = otherwise.checker.split(m2)
+    otherwise.checker.refine_coreachable(m0_lhs.eq(m0_rhs))
+    otherwise.checker.refine_coreachable(m1_lhs.eq(m1_rhs))
+    otherwise.checker.refine_coreachable(m2_lhs.eq(m2_rhs))
