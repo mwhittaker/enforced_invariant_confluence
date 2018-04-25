da6b27ec4996782277f1b582a34054e36793218f
Michael Whittaker <mjwhittaker@berkeley.edu>
2018-04-24 19:51:09 -0700

    Updated plot for new experiments.

--- benchmark/plot.py
+++ benchmark/plot.py
@@ -28,77 +28,98 @@ LIN_FMT = {
 def load_csv(filename):
     with open(filename, 'r') as f:
         reader = csv.reader(f, delimiter=',')
-        return [(row[0], float(row[1]), float(row[2])) for row in reader]
+        return [(row[0], float(row[1]), float(row[2]), float(row[3]))
+                for row in reader]
 
-def split_data(data):
-    splits = defaultdict(lambda: ([], []))
-    for (server_type, x, y) in data:
-        splits[server_type][0].append(x)
-        splits[server_type][1].append(y)
-    return splits
+def extract_bank_account_data(data):
+    records = []
+    for (server_type, num_servers, fraction_withdraw, throughput) in data:
+        records.append({
+            'server_type': server_type,
+            'num_servers': num_servers,
+            'fraction_withdraw': fraction_withdraw,
+            'throughput': throughput,
+        })
+    return records
 
-def plot_vary_withdraws(data):
-    splits = split_data(data)
+def partition_by_server_type(records):
+    gossip = []
+    segmented_master = []
+    paxos = []
+    for r in records:
+        if r['server_type'] == 'gossip':
+            gossip.append(r)
+        elif r['server_type'] == 'segmented_master':
+            segmented_master.append(r)
+        elif r['server_type'] == 'paxos':
+            paxos.append(r)
+        else:
+            raise ValueError(r['server_type'])
+    return gossip, segmented_master, paxos
 
-    gossip_x, gossip_y = splits['gossip']
-    segmented_x, segmented_y = splits['segmented_master']
-    paxos_x, paxos_y = splits['paxos']
+def plot_bank_account(data):
+    records = extract_bank_account_data(data)
+    num_servers_domain = sorted(set(r['num_servers'] for r in records))
+    frac_withdraw_domain = sorted(set(r['fraction_withdraw'] for r in records))
 
-    plt.figure(figsize=(8, 3))
-    plt.semilogy(gossip_x, gossip_y, **EVENTUAL_FMT)
-    plt.semilogy(segmented_x, segmented_y, **IC_FMT)
-    plt.semilogy(paxos_x, paxos_y, **LIN_FMT)
-    plt.legend(loc='upper center', ncol=3, bbox_to_anchor=(0.5, 1.2), prop={'size': 12})
-    plt.xlabel('Decrement Frequency (fraction of workload)')
-    plt.ylabel('Throughput (txns/s)')
-    plt.savefig('vary_withdraws.pdf', bbox_inches='tight')
-    plt.close()
+    # First, we plot the throughput (y-axis) vs fraction withdraw (x-axis) for
+    # each number of servers.
+    def filter_num_servers_sort_fraction(records, n):
+        filtered = [r for r in records if r['num_servers'] == n]
+        ordered = sorted(filtered, key=lambda r: r['fraction_withdraw'])
+        xs = [r['fraction_withdraw'] for r in ordered]
+        ys = [r['throughput'] for r in ordered]
+        return xs, ys
 
-def plot_vary_segments(data):
-    splits = split_data(data)
+    for n in num_servers_domain:
+        gossip, master, paxos = partition_by_server_type(records)
+        gossip_xs, gossip_ys = filter_num_servers_sort_fraction(gossip, n)
+        master_xs, master_ys = filter_num_servers_sort_fraction(master, n)
+        paxos_xs, paxos_ys = filter_num_servers_sort_fraction(paxos, n)
 
-    gossip_x, gossip_y = splits['gossip']
-    segmented_x, segmented_y = splits['segmented_master']
-    paxos_x, paxos_y = splits['paxos']
+        plt.figure(figsize=(8, 3))
+        plt.semilogy(gossip_xs, gossip_ys, **EVENTUAL_FMT)
+        plt.semilogy(master_xs, master_ys, **IC_FMT)
+        plt.semilogy(paxos_xs, paxos_ys, **LIN_FMT)
+        plt.legend(loc='upper center', ncol=3, bbox_to_anchor=(0.5, 1.2), prop={'size': 12})
+        plt.xlabel('Decrement Frequency (fraction of workload)')
+        plt.ylabel('Throughput (txns/s)')
+        plt.savefig(f'throughput_vs_fraction_{n}.pdf', bbox_inches='tight')
+        plt.close()
 
-    plt.figure(figsize=(8, 3))
-    plt.semilogy(gossip_x, gossip_y, **EVENTUAL_FMT)
-    plt.semilogy(segmented_x, segmented_y, **IC_FMT)
-    plt.semilogy(paxos_x, paxos_y, **LIN_FMT)
-    plt.xlabel('Segment Side Length')
-    plt.ylabel('Throughput (txns/s)')
-    plt.savefig('vary_segments.pdf', bbox_inches='tight')
-    plt.close()
+    # First, we plot the throughput (y-axis) vs number of nodes (x-axis) for
+    # each fraction of withdraws.
+    def filter_fraction_sort_num_servers(records, f):
+        filtered = [r for r in records if r['fraction_withdraw'] == f]
+        ordered = sorted(filtered, key=lambda r: r['num_servers'])
+        xs = [r['num_servers'] for r in ordered]
+        ys = [r['throughput'] for r in ordered]
+        return xs, ys
 
-def plot_vary_nodes(data):
-    splits = split_data(data)
+    for f in frac_withdraw_domain:
+        gossip, master, paxos = partition_by_server_type(records)
+        gossip_xs, gossip_ys = filter_fraction_sort_num_servers(gossip, f)
+        master_xs, master_ys = filter_fraction_sort_num_servers(master, f)
+        paxos_xs, paxos_ys = filter_fraction_sort_num_servers(paxos, f)
 
-    gossip_x, gossip_y = splits['gossip']
-    segmented_x, segmented_y = splits['segmented_master']
-    paxos_x, paxos_y = splits['paxos']
-
-    plt.figure(figsize=(8, 4))
-    plt.semilogy(gossip_x, gossip_y, **EVENTUAL_FMT)
-    plt.semilogy(segmented_x, segmented_y, **IC_FMT)
-    plt.semilogy(paxos_x, paxos_y, **LIN_FMT)
-    plt.legend(loc='upper center', ncol=3, bbox_to_anchor=(0.5, 1.15), prop={'size': 12})
-    plt.xlabel('Number of Nodes')
-    plt.ylabel('Throughput (txns/s)')
-    plt.savefig('vary_nodes.pdf', bbox_inches='tight')
-    plt.close()
+        plt.figure(figsize=(8, 4))
+        plt.semilogy(gossip_xs, gossip_ys, **EVENTUAL_FMT)
+        plt.semilogy(master_xs, master_ys, **IC_FMT)
+        plt.semilogy(paxos_xs, paxos_ys, **LIN_FMT)
+        plt.legend(loc='upper center', ncol=3, bbox_to_anchor=(0.5, 1.15), prop={'size': 12})
+        plt.xlabel('Number of Nodes')
+        plt.ylabel('Throughput (txns/s)')
+        plt.savefig(f'throughput_vs_num_nodes_{f}.pdf', bbox_inches='tight')
+        plt.close()
 
 def main():
-    if len(sys.argv) != 4:
-        print('python plot.py <vary_withdraws.csv> <vary_segments.csv> <vary_nodes.csv>')
+    if len(sys.argv) != 2:
+        print('python plot.py <bank_account.csv>')
         sys.exit(1)
 
-    font = {'family' : 'normal',
-            'size'   : 15}
+    font = {'family': 'normal', 'size': 15}
     matplotlib.rc('font', **font)
-
-    plot_vary_withdraws(load_csv(sys.argv[1]))
-    plot_vary_segments(load_csv(sys.argv[2]))
-    plot_vary_nodes(load_csv(sys.argv[3]))
+    plot_bank_account(load_csv(sys.argv[1]))
 
 if __name__ == '__main__':
     main()
